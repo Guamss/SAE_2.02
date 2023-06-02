58a50e0ba100b7752943acabcd21dd4a86c0c77b
Guams <guamsmdr@gmail.com>
2023-06-02 17:17:33 +0200

    (feat): Ajout du contenu de la classe Joueur

--- src/main/java/com/sae/sae2_02/modele/Joueur.java
+++ src/main/java/com/sae/sae2_02/modele/Joueur.java
@@ -14,12 +14,11 @@ public class Joueur extends Position
         this.experience = 0;
         this.time = 0;
     }
-    //methode à finir et à tester
     public int getExperience()
     {
         return experience;
     }
-    //methode à finir et à tester
+
     public int getTime()
     {
         return time;
@@ -27,18 +26,37 @@ public class Joueur extends Position
 
     public void completeQuest(Quete quete, LinkedList<Integer> liste)
     {
-        ;
+        int[] gap = this.posGap(quete.getPosition());
+        if (Arrays.equals(gap, new int[]{0, 0}))
+        {
+            liste.add(quete.numero);
+            time+=quete.duree;
+            if (quete.numero != 0)
+            {
+                experience+=quete.experience;
+            }
+        }
+        else
+        {
+            throw new ExceptionJoueur(0);
+        }
     }
+
     public void move(int[] parPos)
     {
-        ;
+        time+=this.mouvement(parPos);
+        setPosition(parPos);
     }
+
     public int mouvement(int[] parPos)
     {
-        ;
+        int deltaX = Math.abs(this.getPosition()[0] - parPos[0]);
+        int deltaY = Math.abs(this.getPosition()[1] - parPos[1]);
+        return deltaX + deltaY;
     }
+
     public String toString()
     {
-        ;
+        return "Joueur à la position : " + Arrays.toString(this.getPosition()) + " | experience : " + this.experience + " | temps de jeu : " + this.time;
     }
 }
