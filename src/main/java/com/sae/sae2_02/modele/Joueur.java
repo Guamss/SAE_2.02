488ea63236def1ba95dc21633455625ca151ef8c
TeddyDgt <teddy.degat@gmail.com>
2023-06-05 13:07:50 +0200

    (docs): Ajout de la documentation sur la classe Joueur

--- src/main/java/com/sae/sae2_02/modele/Joueur.java
+++ src/main/java/com/sae/sae2_02/modele/Joueur.java
@@ -1,61 +1,86 @@
 package com.sae.sae2_02.modele;
 
-
 import java.util.*;
 
-public class Joueur extends Position
-{
+/**
+ * Classe représentant le joueur qui généralise la classe Position.
+ */
+public class Joueur extends Position {
     private int experience;
     private int[] position;
     private int time;
-    public Joueur()
-    {
+
+    /**
+     * Constructeur par défaut du joueur.
+     * Initialise la position du joueur à [0, 0], l'expérience à 0 et le temps à 0.
+     */
+    public Joueur() {
         super(new int[]{0, 0});
         this.experience = 0;
         this.time = 0;
     }
-    public int getExperience()
-    {
+
+    /**
+     * Obtient l'expérience du joueur.
+     *
+     * @return l'expérience du joueur
+     */
+    public int getExperience() {
         return experience;
     }
 
-    public int getTime()
-    {
+    /**
+     * Obtient le temps du joueur.
+     *
+     * @return le temps du joueur
+     */
+    public int getTime() {
         return time;
     }
 
+    /**
+     * Effectue la complétion d'une quête.
+     *
+     * @param quete  la quête à compléter
+     * @param liste  la liste de quêtes complétées
+     * @throws ExceptionJoueur si le joueur n'est pas à l'emplacement de la quête
+     */
     public void completeQuest(Quete quete, LinkedList<Integer> liste) throws ExceptionJoueur {
         int[] gap = this.posGap(quete.getPosition());
-        if (Arrays.equals(gap, new int[]{0, 0}))
-        {
+        if (Arrays.equals(gap, new int[]{0, 0})) {
             liste.add(quete.numero);
-            time+=quete.duree;
-            if (quete.numero != 0)
-            {
-                experience+=quete.experience;
+            time += quete.duree;
+            if (quete.numero != 0) {
+                experience += quete.experience;
             }
-        }
-        else
-        {
+        } else {
             throw new ExceptionJoueur(0);
         }
     }
 
-    public void move(int[] parPos)
-    {
-        time+=this.mouvement(parPos);
+    /**
+     * Déplace le joueur à une nouvelle position.
+     *
+     * @param parPos la nouvelle position
+     */
+    public void move(int[] parPos) {
+        time += this.mouvement(parPos);
         setPosition(parPos);
     }
 
-    public int mouvement(int[] parPos)
-    {
+    /**
+     * Calcule le nombre de mouvements nécessaires pour atteindre une position donnée.
+     *
+     * @param parPos la position cible
+     * @return le nombre de mouvements nécessaires
+     */
+    public int mouvement(int[] parPos) {
         int deltaX = Math.abs(this.getPosition()[0] - parPos[0]);
         int deltaY = Math.abs(this.getPosition()[1] - parPos[1]);
         return deltaX + deltaY;
     }
-
-    public String toString()
-    {
-        return "Joueur à la position : " + Arrays.toString(this.getPosition()) + " | experience : " + this.experience + " | temps de jeu : " + this.time;
+    
+    public String toString() {
+        return "Joueur à la position : " + Arrays.toString(this.getPosition()) + " | expérience : " + this.experience + " | temps de jeu : " + this.time;
     }
 }
