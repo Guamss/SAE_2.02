49e6b98a184a0e8f13349f15bfa47d1468b3a5e0
TeddyDgt <teddy.degat@gmail.com>
2023-06-05 13:09:00 +0200

    (docs): Ajout de la documentation sur la classe LectureFichierTexte

--- src/main/java/com/sae/sae2_02/modele/LectureFichierTexte.java
+++ src/main/java/com/sae/sae2_02/modele/LectureFichierTexte.java
@@ -1,25 +1,28 @@
 package com.sae.sae2_02.modele;
 
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class LectureFichierTexte
-{
-    public static Scenario lecture(File fichier)
-    {
+/**
+ * Classe permettant de lire un fichier texte contenant les informations d'un scénario.
+ */
+public class LectureFichierTexte {
+
+    /**
+     * Lit le contenu d'un fichier texte et génère un scénario.
+     *
+     * @param fichier le fichier texte à lire
+     * @return le scénario généré à partir du fichier
+     */
+    public static Scenario lecture(File fichier) {
         Scenario scenario = new Scenario();
-        try
-        {
+        try {
             Scanner scanner = new Scanner(fichier);
-            while(scanner.hasNext())
-            {
+            while (scanner.hasNext()) {
                 scenario.ajout(new Quete(scanner.nextLine()));
             }
-        }
-        catch (FileNotFoundException e)
-        {
+        } catch (FileNotFoundException e) {
             System.err.println(e.getMessage());
         }
         return scenario;
