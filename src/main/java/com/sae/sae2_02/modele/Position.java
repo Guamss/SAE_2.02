50ec33048c0621b92f8ee8326bdd03afcad8794a
TeddyDgt <teddy.degat@gmail.com>
2023-06-05 13:10:47 +0200

    (docs): Ajout de la documentation sur la classe Position

--- src/main/java/com/sae/sae2_02/modele/Position.java
+++ src/main/java/com/sae/sae2_02/modele/Position.java
@@ -1,30 +1,50 @@
 package com.sae.sae2_02.modele;
 
-
 import java.util.Arrays;
 
-public class Position
-{
+/**
+ * Classe représentant une position avec des coordonnées [x, y].
+ */
+public class Position {
     protected int[] position;
-    public Position(int[] parPosition)
-    {
+
+    /**
+     * Constructeur de la classe Position.
+     *
+     * @param parPosition un tableau d'entiers représentant les coordonnées (x, y)
+     */
+    public Position(int[] parPosition) {
         this.position = parPosition;
     }
 
-    public int[] getPosition()
-    {
+    /**
+     * Retourne les coordonnées de la position.
+     *
+     * @return un tableau d'entiers représentant les coordonnées (x, y)
+     */
+    public int[] getPosition() {
         return position;
     }
 
-    public int[] posGap(int[] pos)
-    {
+    /**
+     * Calcule l'écart entre la position actuelle et une autre position donnée.
+     *
+     * @param pos les coordonnées de la position à comparer
+     * @return un tableau d'entiers représentant l'écart en valeur absolue entre les coordonnées (x, y)
+     */
+    public int[] posGap(int[] pos) {
         int[] ecartPositions = new int[2];
         ecartPositions[0] = Math.abs(this.getPosition()[0] - pos[0]);
         ecartPositions[1] = Math.abs(this.getPosition()[1] - pos[1]);
         return ecartPositions;
     }
-    public void setPosition(int[] pos)
-    {
+
+    /**
+     * Définit les coordonnées de la position.
+     *
+     * @param pos les nouvelles coordonnées (x, y)
+     */
+    public void setPosition(int[] pos) {
         this.position = pos;
     }
 }
