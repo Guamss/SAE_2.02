478ad0c2943ca34ea1ce02d0f3f45f289eb05eeb
TeddyDgt <teddy.degat@gmail.com>
2023-06-05 11:45:03 +0200

    Ajout des exceptions de la classe Scenario

---
+++ src/test/java/com/sae/sae2_02/tests/AlgorithmeTest.java
@@ -0,0 +1,16 @@
+package com.sae.sae2_02.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AlgorithmeTest {
+
+    @org.junit.jupiter.api.Test
+    void efficace() {
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void exhaustive() {
+
+    }
+}
