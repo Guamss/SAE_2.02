a56d5b5ef1fce92086c0c710cbf9066763d633ee
TeddyDgt <teddy.degat@gmail.com>
2023-06-05 13:02:28 +0200

    (docs): Ajout de la documentation sur la classe ExceptionScenario

--- src/main/java/com/sae/sae2_02/modele/ExceptionScenario.java
+++ src/main/java/com/sae/sae2_02/modele/ExceptionScenario.java
@@ -1,17 +1,29 @@
 package com.sae.sae2_02.modele;
 
-
-public class ExceptionScenario extends Exception implements ConstanteErreur
-{
+/**
+ * Exception personnalisée pour les erreurs liées au scénario.
+ */
+public class ExceptionScenario extends Exception implements ConstanteErreur {
     private int chErrorCode;
-    public ExceptionScenario(int errorCode)
-    {
+
+    /**
+     * Constructeur de l'exception avec le code d'erreur.
+     *
+     * @param errorCode le code d'erreur
+     */
+    public ExceptionScenario(int errorCode) {
         super();
-        chErrorCode=errorCode;
+        chErrorCode = errorCode;
     }
-    public String getError()
-    {
-        return ERREUR_SCENARIO[chErrorCode] + " - Error code : " + chErrorCode;
+
+    /**
+     * Récupère le message d'erreur correspondant au code d'erreur.
+     *
+     * @return le message d'erreur
+     */
+    public String getError() {
+        return ERREUR_SCENARIO[chErrorCode] + " - Error code: " + chErrorCode;
     }
 }
+    
 
