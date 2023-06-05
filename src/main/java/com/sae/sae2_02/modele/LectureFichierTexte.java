598601bf5bcf8b60f2138db3cac26243a5a1eab6
TeddyDgt <teddy.degat@gmail.com>
2023-06-05 12:39:40 +0200

    (feat): Ajout du contenu de la classe LectureFichierTexte

--- src/main/java/com/sae/sae2_02/modele/LectureFichierTexte.java
+++ src/main/java/com/sae/sae2_02/modele/LectureFichierTexte.java
@@ -9,6 +9,19 @@ public class LectureFichierTexte
 {
     public static Scenario lecture(File fichier)
     {
-        ;
+        Scenario scenario = new Scenario();
+        try
+        {
+            Scanner scanner = new Scanner(fichier);
+            while(scanner.hasNext())
+            {
+                scenario.ajout(new Quete(scanner.nextLine()));
+            }
+        }
+        catch (FileNotFoundException e)
+        {
+            System.err.println(e.getMessage());
+        }
+        return scenario;
     }
 }
