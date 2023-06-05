c6457983e2e74edc89f9dfe1e826f43eb0523ccb
TeddyDgt <teddy.degat@gmail.com>
2023-06-05 12:59:47 +0200

    (docs): Ajout de la documentation sur la classe ExceptionAlgorithme

--- src/main/java/com/sae/sae2_02/modele/ExceptionAlgorithme.java
+++ src/main/java/com/sae/sae2_02/modele/ExceptionAlgorithme.java
@@ -1,15 +1,34 @@
 package com.sae.sae2_02.modele;
+
 import static com.sae.sae2_02.modele.ConstanteErreur.ERREUR_ALGO;
 
+/**
+ * Exception personnalisée pour les erreurs liées à l'algorithme.
+ */
 public class ExceptionAlgorithme extends Exception {
     private int chErrorCode;
-    public ExceptionAlgorithme(int errorCode)
-    {
+
+    /**
+     * Constructeur de l'exception avec le code d'erreur.
+     *
+     * @param errorCode le code d'erreur
+     */
+    public ExceptionAlgorithme(int errorCode) {
         super();
-        chErrorCode=errorCode;
+        chErrorCode = errorCode;
     }
-    public String getError()
-    {
-        return ERREUR_ALGO[chErrorCode] + " - Error code : " + chErrorCode;
+
+    /**
+     * Récupère le message d'erreur correspondant au code d'erreur.
+     *
+     * @return le message d'erreur
+     */
+    public String getError() {
+        return ERREUR_ALGO[chErrorCode] + " - Error code: " + chErrorCode;
     }
 }
+
+
+
+
+
