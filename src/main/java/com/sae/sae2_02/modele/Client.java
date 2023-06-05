85df9907fcc0574f239db0573eb88ce8f140b1be
TeddyDgt <teddy.degat@gmail.com>
2023-06-05 12:42:33 +0200

    (refactor): Execution du code via la classe Client

--- src/main/java/com/sae/sae2_02/modele/Client.java
+++ src/main/java/com/sae/sae2_02/modele/Client.java
@@ -7,8 +7,21 @@ import java.util.Arrays;
 
 public class Client
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws ExceptionJoueur
     {
-        ;
+        try
+        {
+            Scenario scenario = LectureFichierTexte.lecture(new File("txt" + File.separator + "scenario_0.txt"));
+            System.out.println("Exhaustive : " + Algorithme.exhaustive(scenario));
+            System.out.println("Efficace : " + Algorithme.efficace(scenario));
+        }
+        catch (ExceptionScenario exception)
+        {
+            System.out.println(exception.getError());
+        }
+        catch (ExceptionAlgorithme exception)
+        {
+            System.out.println(exception.getError());
+        }
     }
 }
