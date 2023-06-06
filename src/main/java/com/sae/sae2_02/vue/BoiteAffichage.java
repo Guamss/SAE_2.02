2f79603034c479f0637abc73eaf0395359b36861
Guams <guamsmdr@gmail.com>
2023-06-06 16:26:23 +0200

    (docs): Ajout de la documentation de la classe BoiteAffichage

--- src/main/java/com/sae/sae2_02/vue/BoiteAffichage.java
+++ src/main/java/com/sae/sae2_02/vue/BoiteAffichage.java
@@ -8,28 +8,40 @@ import java.util.LinkedList;
 import java.util.Set;
 import java.util.TreeMap;
 
-public class BoiteAffichage extends VBox
-{
+/**
+ * Classe représentant une boîte d'affichage dans l'application.
+ * Cette boîte affiche les solutions et leurs informations associées.
+ */
+public class BoiteAffichage extends VBox {
     private Label solutionText = new Label("Veuillez choisir un scénario");
     private Label nbrSolution = new Label("");
     private Label typeSolution = new Label("");
-    public BoiteAffichage()
-    {
+
+    /**
+     * Constructeur de la classe BoiteAffichage.
+     * Initialise les labels de la boîte d'affichage et configure le style.
+     */
+    public BoiteAffichage() {
         nbrSolution.setId("solution");
         solutionText.setId("solution");
         typeSolution.setId("solution");
         this.setPadding(new Insets(20));
         this.getChildren().addAll(typeSolution, solutionText, nbrSolution);
     }
-    public void afficher(TreeMap<Integer, LinkedList<Integer>> map, String type)
-    {
+
+    /**
+     * Affiche les solutions dans la boîte d'affichage.
+     *
+     * @param map  Une TreeMap contenant les solutions, avec les clés représentant les unités de temps et les valeurs représentant les chemins.
+     * @param type Le type de solution (efficace, exhaustive...).
+     */
+    public void afficher(TreeMap<Integer, LinkedList<Integer>> map, String type) {
         typeSolution.setText(type + " :\n----------");
         Set<Integer> keys = map.keySet();
         int counter = 1;
         String content = "";
-        for (int key : keys)
-        {
-            content += counter + ") Le joueur a mit " + Integer.toString(key) + " unités de temps. \n" + "Chemin : " + map.get(key) + "\n----------" + "\n";
+        for (int key : keys) {
+            content += counter + ") Le joueur a mis " + Integer.toString(key) + " unités de temps. \n" + "Chemin : " + map.get(key) + "\n----------" + "\n";
         }
         solutionText.setText(content);
         nbrSolution.setText("Nombre de solution : " + map.size());
