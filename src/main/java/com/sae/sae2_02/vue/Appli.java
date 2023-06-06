d682683984ff8f518863678177c15419cb3ad847
Guams <guamsmdr@gmail.com>
2023-06-06 16:26:05 +0200

    (docs): Ajout de la documentation de la classe Appli

--- src/main/java/com/sae/sae2_02/vue/Appli.java
+++ src/main/java/com/sae/sae2_02/vue/Appli.java
@@ -7,21 +7,29 @@ import javafx.stage.Stage;
 
 import java.io.File;
 
-public class Appli extends Application
-{
+/**
+ * Classe principale de l'application Appli.
+ * Elle généralise la classe Application de JavaFX et
+ * définit la méthode principale pour démarrer l'application.
+ */
+public class Appli extends Application {
+    /**
+     * Méthode principale qui démarre l'application.
+     *
+     * @param stage La fenêtre principale de l'application.
+     * @throws Exception En cas d'erreur lors du démarrage de l'application.
+     */
     @Override
-    public void start(Stage stage) throws Exception
-    {
+    public void start(Stage stage) throws Exception {
         stage.setTitle("SAE - Exploration Algorithmique");
         VBox root = new VBoxRoot();
-        Scene scene = new Scene(root,600,200);
+        Scene scene = new Scene(root, 600, 200);
         File fichierCss = new File("css" + File.separator + "style.css");
         scene.getStylesheets().add(fichierCss.toURI().toString());
         stage.setScene(scene);
         stage.show();
     }
-    public static void main(String[] args)
-    {
-       Application.launch(args);
+    public static void main(String[] args) {
+        Application.launch(args);
     }
 }
