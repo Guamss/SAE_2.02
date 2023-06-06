74526dfc50c388426e84754211efd70281284352
Guams <guamsmdr@gmail.com>
2023-06-06 16:27:01 +0200

    (docs): Ajout de la documentation de l'interface ConstanteScenario

--- src/main/java/com/sae/sae2_02/vue/ConstanteScenario.java
+++ src/main/java/com/sae/sae2_02/vue/ConstanteScenario.java
@@ -1,19 +1,23 @@
 package com.sae.sae2_02.vue;
 
-public interface ConstanteScenario
-{
-    final String[] SCENARIOS =
-            {
-                    "scenario_0.txt",
-                    "scenario_1.txt",
-                    "scenario_2.txt",
-                    "scenario_3.txt",
-                    "scenario_4.txt",
-                    "scenario_5.txt",
-                    "scenario_6.txt",
-                    "scenario_7.txt",
-                    "scenario_8.txt",
-                    "scenario_9.txt",
-                    "scenario_10.txt"
-            };
+/**
+ * Interface contenant la constante SCENARIOS
+ */
+public interface ConstanteScenario {
+    /**
+     * Tableau de chaînes de caractères représentant les noms des fichiers de scénarios.
+     */
+    final String[] SCENARIOS = {
+            "scenario_0.txt",
+            "scenario_1.txt",
+            "scenario_2.txt",
+            "scenario_3.txt",
+            "scenario_4.txt",
+            "scenario_5.txt",
+            "scenario_6.txt",
+            "scenario_7.txt",
+            "scenario_8.txt",
+            "scenario_9.txt",
+            "scenario_10.txt"
+    };
 }
